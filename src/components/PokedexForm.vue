74556925dc49187aeb3d33a56b862cda8dc17a1b
Bryan Cordero <bryan_cp11@hotmail.com>
2023-05-09 17:03:05 -0600

    First commit of vue Pokedex

---
+++ src/components/PokedexForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form class="pokemon-form" method="dialog">
+        <input
+        class="pokemon-input"
+        type="text"
+        name="pokemon"
+        v-model="pokemonName"
+        placeholder="Busca tu pokemon"
+        autocomplete="off"/>
+        <button @click="sendId" class="pokemon-btn">Enviar</button>
+    </form>
+</template>
+
+<script>
+    
+export default {
+    name: 'PokedexForm',
+
+    data () {
+        return {
+        pokemonName: ''
+        }
+    },
+    emits: ['response'],
+    methods: {
+        sendId () {
+            const newPokemonId = window.isNaN(parseInt(this.pokemonName)) ? this.pokemonName.toLowerCase() : this.pokemonName
+            this.$emit('response', newPokemonId)
+        }
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import PokeDex from './PokeDex.vue'
+
+createApp(PokeDex).mount('#app')
